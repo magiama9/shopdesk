12d2304eaab3cfc7a00a32ecbebcc7a2f0916e5d
Katie Durga <katie.durga@gmail.com>
2020-02-11 14:00:23 -0500

    database skeleton

---
+++ schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS coolStoreDB;
+
+-- Create the database employeeDB and specified it for use.
+CREATE DATABASE coolStoreDB;
+
+USE coolStoreDB;
+
+CREATE TABLE items (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  price INT(5),
+  desc VARCHAR(100),
+  img VARCHAR(500),
+  inCart BOOLEAN DEFAULT false,
+  saved BOOLEAN DEFAULT false,
+  PRIMARY KEY(id)
+)
