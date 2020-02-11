722f9910d263d0d465296e374e5501cc4dcc2913
Sam Randels <samrandels@gmail.com>
2020-02-11 16:09:20 -0500

    Change force:type to force:true

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 //requires in index file that sequelize helps us with (DO NOT TOUCH THAT INDEX.JS FILE GOD DAMNIT!)
 const db = require("./models");
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(express.static("app/public"));
 //Routes
 require("./app/routes/api-routes.js")(app);
-//force:type is essentially the same as DROP DATABASE IF EXISTS
-db.sequelize.sync({ force: type }).then(function() {
+//force:true is essentially the same as DROP DATABASE IF EXISTS
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("Listening on port %s", PORT);
   });
