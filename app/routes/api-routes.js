27b17156a3b63c8337558c2f3c883a61289d4034
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-02-12 11:44:46 -0500

    Add multer and set file storage

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,32 +1,43 @@
+// =============================================================
+//                  SET UP IMAGE UPLOADING
+//              https://www.npmjs.com/package/multer
+// =============================================================
+const multer = require("multer");
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "../public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.filename + "-" + Date.now());
+  }
+});
 
+const upload = multer({ storage: storage });
 
 // =============================================================
 //                  REQUIRE IN QUERY FUNCTIONS
 // =============================================================
 
-const query = require("../../db/lib/query")
+const query = require("../../db/lib/query");
 
 // =============================================================
 //                          ROUTES
 // =============================================================
 
 module.exports = function(app) {
-
-
   // GET ROUTE FOR VIEWING INVENTORY
 
-
   // POST ROUTE FOR ADDING INVENTORY
-
+  // FORM NEEDS TO HAVE A FILE INPUT FIELD NAMED PHOTO
+  app.post("/inventory/upload", upload.single("photo"), (req, res, next) => {
+    // req.file is the `photo` file
+    // req.body will hold the text fields of the form
+  });
 
   // PUT ROUTE FOR UPDATING INVENTORY QUANTITY
 
-  
   // PUT ROUTE FOR ADDING TO CART
 
-
   // PUT ROUTE FOR ADDING TO SAVED ITEMS
-
-  
-}
+};
