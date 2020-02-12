fdaa5f0856d9935f59353ee000bf7fd85bebef28
Anna Panas <annapanas0906@gmail.com>
2020-02-12 12:17:04 -0500

    added 'home' to navbar, added classes to hide and show content when clicked

--- public/assets/layout.js
+++ public/assets/layout.js
@@ -1,4 +1,12 @@
+// if any of the following buttons (kitchen, bedroom, bathroom, living room, outdoors) klicke
+//initial  picture will hide and a card with items will display
 $(".menu").on("click", function () {
 $(".initial").addClass("hide");
 $(".product-display").removeClass("hide");
+});
+
+// if home page is cliked, hides items card and displays initial picture
+$(".home").on("click", function () {
+  $(".product-display").addClass("hide");
+  $(".initial").removeClass("hide");
 });
