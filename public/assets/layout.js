88b9160c099e467a81147007393819dd4298799d
Anna Panas <annapanas0906@gmail.com>
2020-02-13 03:15:31 -0500

    payment/billing form done. Added comfirmation order card

--- public/assets/layout.js
+++ public/assets/layout.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-	$('select').formSelect(); // materialize plugin for form where user is selecting state
+  $('select').formSelect(); // materialize plugin for form where user is selecting state
 });
 
 // All the content lives on one html page, so we're adding hide/show classes for all links and buttons
 
 // Class menu added to these buttons: kitchen, bedroom, bathroom, living room, outdoors
 $(".menu").on("click", function () {
-$(".initial").addClass("hide");
-$(".product-display").removeClass("hide");
-$(".cart").addClass("hide");
+  $(".initial").addClass("hide");
+  $(".product-display").removeClass("hide");
+  $(".cart").addClass("hide");
 });
 
 // home page icon on a navbar
@@ -19,14 +19,14 @@ $(".home").on("click", function () {
 });
 
 // shopping-cart icon on a navbar
-$(".shopping-cart").on("click", function (){
-    $(".product-display").addClass("hide");
-    $(".initial").addClass("hide");
-    $(".cart").removeClass("hide");
+$(".shopping-cart").on("click", function () {
+  $(".product-display").addClass("hide");
+  $(".initial").addClass("hide");
+  $(".cart").removeClass("hide");
 });
 
 // Proceed to checkout button
-$(".chekout-btn").on("click", function (){
+$(".chekout-btn").on("click", function () {
   $(".cart").addClass("hide");
   $(".product-display").addClass("hide");
   $(".initial").addClass("hide");
@@ -41,4 +41,25 @@ $(".add-to-cart").on("click", function () {
   $(".checkout-form").addClass("hide");
 });
 
+// checkout button
+$(".payment-btn").on("click", function (){
+  $(".checkout-form").addClass("hide");
+  $(".payment-form").removeClass("hide");
+  $(".cart").addClass("hide");
 
+});
+
+// place order button
+$(".place-order").on("click", function(){
+  $(".product-display").addClass("hide");
+  $(".initial").addClass("hide");
+  $(".checkout-form").addClass("hide");
+  $(".payment-form").addClass("hide");
+  $(".thank-you").removeClass("hide");
+});
+
+// continue-shopping
+$(".continue-shopping").on("click", function (){
+  $(".thank-you").addClass("hide");
+  $(".initial").removeClass("hide");
+});
