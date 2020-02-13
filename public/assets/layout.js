1cd4bf33a12667bd3931a3036b90ade8954b1624
Anna Panas <annapanas0906@gmail.com>
2020-02-13 01:15:15 -0500

    added shipping info form

--- public/assets/layout.js
+++ public/assets/layout.js
@@ -1,22 +1,44 @@
-// if any of the following buttons (kitchen, bedroom, bathroom, living room, outdoors) klicked
-//home page hides and a card with items will display
+$(document).ready(function () {
+	$('select').formSelect(); // materialize plugin for form where user is selecting state
+});
+
+// All the content lives on one html page, so we're adding hide/show classes for all links and buttons
+
+// Class menu added to these buttons: kitchen, bedroom, bathroom, living room, outdoors
 $(".menu").on("click", function () {
 $(".initial").addClass("hide");
 $(".product-display").removeClass("hide");
 $(".cart").addClass("hide");
 });
 
-// if home icon is cliked, hides items card and displays home page
+// home page icon on a navbar
 $(".home").on("click", function () {
   $(".product-display").addClass("hide");
   $(".initial").removeClass("hide");
   $(".cart").addClass("hide");
 });
 
-// if shopping cart cliked,
+// shopping-cart icon on a navbar
 $(".shopping-cart").on("click", function (){
     $(".product-display").addClass("hide");
     $(".initial").addClass("hide");
     $(".cart").removeClass("hide");
-    
-})
+});
+
+// Proceed to checkout button
+$(".chekout-btn").on("click", function (){
+  $(".cart").addClass("hide");
+  $(".product-display").addClass("hide");
+  $(".initial").addClass("hide");
+  $(".checkout-form").removeClass("hide");
+});
+
+// add-to cart button
+$(".add-to-cart").on("click", function () {
+  $(".cart").removeClass("hide");
+  $(".product-display").addClass("hide");
+  $(".initial").addClass("hide");
+  $(".checkout-form").addClass("hide");
+});
+
+
