e6ca0638b78bfc0bfe9587a375ca90686ab49d29
Anna Panas <annapanas0906@gmail.com>
2020-02-12 22:36:25 -0500

    designed shopping cart page

--- public/assets/layout.js
+++ public/assets/layout.js
@@ -1,12 +1,22 @@
-// if any of the following buttons (kitchen, bedroom, bathroom, living room, outdoors) klicke
-//initial  picture will hide and a card with items will display
+// if any of the following buttons (kitchen, bedroom, bathroom, living room, outdoors) klicked
+//home page hides and a card with items will display
 $(".menu").on("click", function () {
 $(".initial").addClass("hide");
 $(".product-display").removeClass("hide");
+$(".cart").addClass("hide");
 });
 
-// if home page is cliked, hides items card and displays initial picture
+// if home icon is cliked, hides items card and displays home page
 $(".home").on("click", function () {
   $(".product-display").addClass("hide");
   $(".initial").removeClass("hide");
+  $(".cart").addClass("hide");
 });
+
+// if shopping cart cliked,
+$(".shopping-cart").on("click", function (){
+    $(".product-display").addClass("hide");
+    $(".initial").addClass("hide");
+    $(".cart").removeClass("hide");
+    
+})
