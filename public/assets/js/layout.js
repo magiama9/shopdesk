a741d2dc32df84d71ec07f281e155b1d31b930ec
Anna Panas <annapanas0906@gmail.com>
2020-02-14 12:11:48 -0500

    added upload product form

--- public/assets/js/layout.js
+++ public/assets/js/layout.js
@@ -14,6 +14,7 @@ $(".menu").on("click", function () {
   $(".checkout-form").addClass("hide");
   $(".payment-form").addClass("hide");
   $(".thank-you").addClass("hide");
+  $(".signup-form").addClass("hide");
 });
 
 // home page icon/or "home" on a navbar
@@ -25,6 +26,7 @@ $(".home").on("click", function () {
   $(".checkout-form").addClass("hide");
   $(".payment-form").addClass("hide");
   $(".thank-you").addClass("hide");
+  $(".signup-form").addClass("hide");
 });
 
 // shopping-cart icon/ or "Your cart" on a navbar
@@ -33,43 +35,52 @@ $(".shopping-cart").on("click", function () {
   $(".initial").addClass("hide");
   $(".cart").removeClass("hide");
   $(".login-form").addClass("hide");
+  $(".signup-form").addClass("hide");
 });
 
 // Proceed to checkout button
 $(".chekout-btn").on("click", function () {
+  window.scrollTo(0,0);
   $(".checkout-form").removeClass("hide");
   $(".cart").addClass("hide");
   $(".product-display").addClass("hide");
   $(".initial").addClass("hide");
   $(".login-form").addClass("hide");
+  $(".signup-form").addClass("hide");
 });
 
 // add to cart button
 $(".add-to-cart").on("click", function () {
+  window.scrollTo(0,0);
   $(".cart").removeClass("hide");
   $(".product-display").addClass("hide");
   $(".initial").addClass("hide");
   $(".checkout-form").addClass("hide");
   $(".login-form").addClass("hide");
   $(".payment-form").addClass("hide");
+  $(".signup-form").addClass("hide");
 });
 
 // Proceed to checkout button
 $(".payment-btn").on("click", function (){
+  window.scrollTo(0,0);
   $(".checkout-form").addClass("hide");
   $(".payment-form").removeClass("hide");
   $(".cart").addClass("hide");
   $(".login-form").addClass("hide");
+  $(".signup-form").addClass("hide");
 });
 
 // place order button
 $(".place-order").on("click", function(){
+  window.scrollTo(0,0);
   $(".product-display").addClass("hide");
   $(".initial").addClass("hide");
   $(".checkout-form").addClass("hide");
   $(".payment-form").addClass("hide");
   $(".thank-you").removeClass("hide");
   $(".login-form").addClass("hide");
+  $(".signup-form").addClass("hide");
 });
 
 // continue-shopping 
@@ -87,10 +98,36 @@ $(".checkout-form").addClass("hide");
 $(".payment-form").addClass("hide");
 $(".thank-you").addClass("hide");
 $(".cart").addClass("hide");
+$(".signup-form").addClass("hide");
 });
 
 
+// login button
+$(".login-btn").on("click", function () {
+  $(".cart").removeClass("hide");
+  $(".login-form").addClass("hide");
+  $(".thank-you").addClass("hide");
+  $(".signup-form").addClass("hide");
+});
 
+// signup icon/ or "sign up" on a navbar
+$(".signup").on("click", function (){
+  $(".signup-form").removeClass("hide");
+  $(".login-form").addClass("hide");
+  $(".product-display").addClass("hide");
+  $(".initial").addClass("hide");
+  $(".checkout-form").addClass("hide");
+  $(".payment-form").addClass("hide");
+  $(".thank-you").addClass("hide");
+  $(".cart").addClass("hide");
+});
+
+$(".signupbtn").on("click", function () {
+  window.scrollTo(0,0);
+  $(".signup-form").addClass("hide");
+  $(".initial").removeClass("hide");
+  $(".thank-you").addClass("hide");
+});
 
 });
 
