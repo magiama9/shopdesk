ef0d6be6aaf89db468a066939f4cc82226fed6f7
Katie Durga <katie.durga@gmail.com>
2020-02-18 10:16:01 -0500

    changed prices to decimals

--- models/items.js
+++ models/items.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   const Items = sequelize.define("Items", {
     name: DataTypes.STRING,
-    price: DataTypes.DECIMAL,
+    price: DataTypes.DECIMAL(10, 2),
     description: DataTypes.STRING,
     qty: DataTypes.INTEGER,
     category: DataTypes.STRING,
