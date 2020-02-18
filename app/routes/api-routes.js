69399a536665d947a6c8f9f7dce26826b8248fb4
Sam Randels <samrandels@gmail.com>
2020-02-17 23:51:22 -0500

    Remove auth from post route

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -106,55 +106,50 @@ module.exports = function(app) {
 
   // POST ROUTE FOR ADDING INVENTORY
   // ROUTE IS SECURED. POST WILL FAIL IF USER IS NOT LOGGED IN
-  app.post(
-    "/inventory/upload",
-    secured(),
-    upload.single("photo"),
-    (req, res, next) => {
-      // req.file is the `photo` file
-      // req.body holds the text fields of the form
+  app.post("/inventory/upload", upload.single("photo"), (req, res, next) => {
+    // req.file is the `photo` file
+    // req.body holds the text fields of the form
 
-      // stores the req.body as a new object
-      let obj = req.body;
-      console.log(req.file.location);
-      // initializes filepath variable
-      let filepath;
+    // stores the req.body as a new object
+    let obj = req.body;
+    console.log(req.file.location);
+    // initializes filepath variable
+    let filepath;
 
-      // Only performs operations on the file path if the file exists
-      // if (typeof req.file !== "undefined") {
-      //   // Returns the file path on windows machines that use the \ for filepaths
-      //   if (
-      //     req.file.path.substring(
-      //       req.file.path.toLowerCase().lastIndexOf("\\assets\\")
-      //     ) != -1
-      //   ) {
-      //     filepath = req.file.path.substring(
-      //       req.file.path.toLowerCase().lastIndexOf("\\assets\\")
-      //     );
-      //   } else if (
-      //     // Returns the correct file path on mac/unix systems that use / for filepaths
-      //     req.file.path.substring(
-      //       req.file.path.toLowerCase().lastIndexOf("/assets/")
-      //     ) != -1
-      //   ) {
-      //     filepath = req.file.path.substring(
-      //       req.file.path.toLowerCase().lastIndexOf("/assets/")
-      //     );
-      //   }
-      // }
+    // Only performs operations on the file path if the file exists
+    // if (typeof req.file !== "undefined") {
+    //   // Returns the file path on windows machines that use the \ for filepaths
+    //   if (
+    //     req.file.path.substring(
+    //       req.file.path.toLowerCase().lastIndexOf("\\assets\\")
+    //     ) != -1
+    //   ) {
+    //     filepath = req.file.path.substring(
+    //       req.file.path.toLowerCase().lastIndexOf("\\assets\\")
+    //     );
+    //   } else if (
+    //     // Returns the correct file path on mac/unix systems that use / for filepaths
+    //     req.file.path.substring(
+    //       req.file.path.toLowerCase().lastIndexOf("/assets/")
+    //     ) != -1
+    //   ) {
+    //     filepath = req.file.path.substring(
+    //       req.file.path.toLowerCase().lastIndexOf("/assets/")
+    //     );
+    //   }
+    // }
 
-      // // replaces \ globally with / for storing in the db
-      // let filepath2 = filepath.replace(/\\/g, "/");
+    // // replaces \ globally with / for storing in the db
+    // let filepath2 = filepath.replace(/\\/g, "/");
 
-      // // Sets the img property of the new object to a filepath
-      // obj.img = filepath2;
+    // // Sets the img property of the new object to a filepath
+    // obj.img = filepath2;
 
-      obj.img = req.file.location;
-      // Adds an item to the database.
-      query.addItem(obj);
-      res.redirect("/inventory");
-    }
-  );
+    obj.img = req.file.location;
+    // Adds an item to the database.
+    query.addItem(obj);
+    res.redirect("/inventory");
+  });
 
   // PUT ROUTE FOR UPDATING INVENTORY QUANTITY
   app.put("/api/inventory/:id", function(req, res) {
